e371b86ce5277558bd5da1ae0557ce371dcf2929
Anya Brun <tsbrun@icloud.com>
2020-07-27 21:03:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,11 +5,5 @@ def prime?(number)
     # if yes then true
     # if no then false
   prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
-  prime_numbers.each do |prime_number|
-    if number % prime_number == 0
-      return false
-    else
-      return true
-    end
-  end
+  prime_numbers.include?(number)
 end
